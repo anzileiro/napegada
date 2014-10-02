80d9d3120c9ee211c90d5320bfbb15c18bfd2948
anzileiro <anderson.anzileiro@gmail.com>
2014-10-02 15:47:43 -0300

    validações login e registro (client side)

--- NaPegada.Web/Scripts/napegada.js
+++ NaPegada.Web/Scripts/napegada.js
@@ -1,7 +1,24 @@
 ﻿///<reference path="http://code.jquery.com/jquery-1.9.1.js" />
 
 $(function () {
+
+    $('#btn-modal-entrar').on('click', function () {
+        f.resetar(['.msg', '.load', '.form-control'], true);
+    });
+
+    $('#btn-modal-registrar').on('click', function () {
+        f.resetar(['.msg', '.load', '.form-control'], true);
+    });
+
     $('#btn-entrar').on('click', function () {
+        if (!f.patternEmail($('#u-email').val())) {
+            $('.msg').html(f.alerta('alert alert-warning alert-dismissible', 'Ops! Digite um email válido. Ex: fulano.tal@mail.com'));
+            return false;
+        }
+        if ($('#u-senha').val() == '') {
+            $('.msg').html(f.alerta('alert alert-warning alert-dismissible', 'Ops! Digite sua senha.'));
+            return false;
+        }
         var retornoJson = undefined;
         $.ajax({
             url: '/Usuario/Entrar',
@@ -13,13 +30,13 @@ $(function () {
             },
             complete: function () {
                 f.exibirLoad('.load', false)
-                f.redirecionar(retornoJson);
+                if (retornoJson == '/Site/Home') {
+                    $('.msg').html(f.alerta('alert alert-warning alert-dismissible', 'Ops! Usuario ou senha são inválidos.'));
+                } else {
+                    f.redirecionar(retornoJson);
+                }
             }
         });
-    });
-
-    $('#btn-sair').on('click', function () {
-        f.obter('/Usuario/Sair', undefined, function (r) { f.redirecionar(r.url) });
     });
 
     $('#btn-registrar').on('click', function () {
@@ -33,13 +50,17 @@ $(function () {
                 f.postar('/Usuario/Entrar', data_ = dados, function (r) {
                     f.exibirLoad('.load', false);
                     f.redirecionar(r.url);
-
                 });
             }
         });
     });
+
+    $('#btn-sair').on('click', function () {
+        f.obter('/Usuario/Sair', undefined, function (r) { f.redirecionar(r.url) });
+    });
 });
 
+//funções
 var f = {
     'ajax': function (url_, type_, data_, beforeSend_, success_, complete_, error_) {
         $.ajax({
@@ -61,11 +82,26 @@ var f = {
     'redirecionar': function (url_) {
         return window.location.href = url_;
     },
-    'exibirLoad': function (elemento_, mostrar_) {
-        if (mostrar_) {
-            $(elemento_).html('<img src="/Content/images/load-100x9.gif" />');
+    'exibirLoad': function (elemento, mostrar) {
+        if (mostrar) {
+            $(elemento).html('<img src="/Content/images/load-100x9.gif" />');
         } else {
-            $(elemento_).html('');
+            $(elemento).html('');
         }
+    },
+    'alerta': function (tipo, msg) {
+        return '<div class="' + tipo + '" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button><strong></strong>' + msg + '</div>';
+    },
+    'resetar': function (elementos, resetar) {
+        if (resetar) {
+            $.each(elementos, function (k, v) {
+                $(v).html('');
+                $(v).val('');
+            });
+        }
+    },
+    'patternEmail': function (dados) {
+        var pattern = new RegExp(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/);
+        return pattern.test(dados);
     }
 };
