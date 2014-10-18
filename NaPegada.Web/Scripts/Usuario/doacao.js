87906d5670cce70ca6b3a6b6d9d87e50f1a9c939
Nicholas Brandao <nicholas.brandao1@gmail.com>
2014-10-18 13:58:15 -0300

    Update da tela home para trazer as doacoes cadastradas, falta funcionar a opcao de detalhes

---
+++ NaPegada.Web/Scripts/Usuario/doacao.js
@@ -0,0 +1,25 @@
+﻿///<reference path="http://code.jquery.com/jquery-1.9.1.js" />
+
+$(function () {
+    $('.abrir-doacao').click(function (e) {
+        $.get($(this).data('url'), abrirForm)
+
+        e.preventDefault();
+        return false;
+    });
+
+    var abrirForm = function (data) {
+        $('#doacao').html(data);
+        $('#modal-doacao').modal('show');
+    }
+
+    $('.excluir-doacao').click(function (e) {
+        $.get($(this).data('url'), function (data) {
+            $('#exclusao').html(data);
+            $('#modal-deletar-doacao').modal('show');
+        })
+
+        e.preventDefault();
+        return false;
+    });
+});
