67c576e9580f26ca07fd8420d31dddfaaeff9224
Michel Barbosa <michel92@bitmessage.ch>
2014-10-18 16:35:21 -0300

    new (PARCIAL): inicio da implementacao do processo de adocao

---
+++ NaPegada.Web/Scripts/Site/_descricao-animal.js
@@ -0,0 +1,17 @@
+﻿///<reference path="http://code.jquery.com/jquery-1.9.1.js" />
+
+$(function () {
+    $('#adotar').click(function () {
+        $('#adocao').hide(200);
+        $('#confirmacao-adocao').show(200);
+    });
+
+    $('#cancelar-adocao').click(function () {
+        $('#confirmacao-adocao').hide(200);
+        $('#adocao').show(200);        
+    });
+
+    $('#confirmar-adocao').click(function () {
+        alert('ae');
+    });
+});
